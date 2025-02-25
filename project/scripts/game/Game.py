093d58f85242d4d9e87b384961c66d0ab388af78
chevitaresebruno <chevitarese.bruno@gmail.com>
2025-02-25 11:06:56 -0300

    Update Game Class for new Concenpts

--- project/scripts/game/Game.py
+++ project/scripts/game/Game.py
@@ -3,14 +3,25 @@ from __init__ import *
 
 from scripts.time.Ticker import Ticker
 from .window.Window import Window
+from .events.EventController import EventController
+from .events.ExitAction import ExitAction
+
+from .mechanics.interactives.movement.TopDownMovement import TopDownMovement
 
 from scripts.test.BlockStates import BlockStates
 
+# TESTS
+from scripts.test.BlockImage import BlockImage
+from scripts.tools.Colors import RED
 
 class Game:
     def __init__(self, fps: int, windowSize: tuple[int, int]):
         self.__ticker = Ticker(fps)
         self.__window = Window(windowSize)
+        self.__eventController = EventController()
+        self.__looper = ExitAction()
+
+        self.__eventController.loadEvent(self.__looper.toLoad())
         
     @staticmethod
     def init() -> None:
@@ -23,8 +34,17 @@ class Game:
     def mainLoop(self, block, spriteGroup) -> None:
         self.__ticker.update()
         self.__window.load(SCENES_DIR / "test.xml")
+
+        mov = TopDownMovement((0, 0))
+        _block = BlockImage.build(RED, (20, 20))
+        mov._TopDownMovement__rigidyBody.rect = _block.get_rect()
+
+        self.__eventController.loadMechanic(mov)
         
-        while True:
+        while self.__looper.infinityLoop:
+            self.__eventController.loop()
+
+            """
             for e in pg.event.get():
                 if (e.type == pg.QUIT):
                     return
@@ -34,10 +54,14 @@ class Game:
                             block.swapState(BlockStates.RGB)
                         case pg.K_c:
                             block.swapState(BlockStates.CMYK)
+            """
 
             self.__window.draw()
+            self.__window.surface.blit(_block, mov._TopDownMovement__rigidyBody.rect)
             
+            """
             spriteGroup.update()
             spriteGroup.draw(self.__window.surface)
-            
+            """
+
             pg.display.flip()
